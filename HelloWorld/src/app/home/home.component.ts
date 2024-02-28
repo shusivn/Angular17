3229b7af900c50b3301ae45a911ced8a7ed581e9
ShusivnTek <shusivntek@gmail.com>
2024-02-28 16:31:51 +0700

    Commit 3

--- HelloWorld/src/app/home/home.component.ts
+++ HelloWorld/src/app/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public name = 'Lộc Ngô';
-  public age = 30;
+  public age = 17.091834646;
   public thuonghieus =[
     'Toyota', 'Honda', 'Ford', 'Audi'
   ];
-
+  public tangTuoi() {
+    this.age++;
+    // if(this.age===20){
+      // this.name = "Hi hi ha ha";
+    // }
+    this.thuonghieus.push(this.name + ' ' + this.age)
+  }
 }
